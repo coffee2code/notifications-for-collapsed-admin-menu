2e90c6b3f8f89e12556de5d67b3204db80cc1330
Scott Reilly <scott@coffee2code.com>
2017-02-23 20:14:00 -0700

    Remove pre-WP 4.6 JS

--- notifications-for-collapsed-admin-menu.js
+++ notifications-for-collapsed-admin-menu.js
@@ -1,13 +1,14 @@
 function c2c_maybe_highlight_comments_icon() {
-	var target = jQuery('#awaiting-mod:first .pending-count'); // WP < 3.2
+	var target = jQuery('.awaiting-mod:first .pending-count');
 	if (! target.length)
-		target = jQuery('.awaiting-mod:first .pending-count');
+		return;
 	var parent = target.parents('#menu-comments');
 	var css_class = 'collapsed-with-pending';
 	var i = target.text();
+
 	i > 0 ?
-		parent.addClass(css_class).attr('title', i+' pending') :
-		parent.removeClass(css_class).attr('title', '');
+		parent.addClass(css_class) :
+		parent.removeClass(css_class);
 }
 function c2c_maybe_highlight_plugins_icon() {
 	var target = jQuery('.plugin-count:first');
