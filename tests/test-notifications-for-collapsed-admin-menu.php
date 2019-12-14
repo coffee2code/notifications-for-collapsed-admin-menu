24f2ed09f62a56f2f9ca346ce39f0d16554c4f83
Scott Reilly <scott@coffee2code.com>
2019-12-14 00:35:07 -0800

    Unit tests: Add tests for `add_css()` and `enqueue_js()`
    Rounds out testing of all plugin functionality.

--- tests/test-notifications-for-collapsed-admin-menu.php
+++ tests/test-notifications-for-collapsed-admin-menu.php
@@ -4,11 +4,40 @@ defined( 'ABSPATH' ) or die();
 
 class Notifications_for_Collapsed_Admin_Menu_Test extends WP_UnitTestCase {
 
+	public function tearDown() {
+		wp_dequeue_script( 'notifications-for-collapsed-admin-menu' );
+	}
+
+
+	//
+	//
+	// DATA PROVIDERS
+	//
+	//
+
+	public static function get_theme_colors() {
+		return array(
+			array( 'fresh', '#444' ),
+			array( 'light', '#ccc' ),
+			array( 'default', '#7c7976' ),
+			array( 'unknown', '#7c7976' ),
+		);
+	}
+
+
 	//
 	//
 	// HELPER FUNCTIONS
 	//
 	//
+
+	private function create_user( $role, $set_as_current = true ) {
+		$user_id = $this->factory->user->create( array( 'role' => $role ) );
+		if ( $set_as_current ) {
+			wp_set_current_user( $user_id );
+		}
+		return $user_id;
+	}
 
 
 	//
@@ -32,6 +61,34 @@ class Notifications_for_Collapsed_Admin_Menu_Test extends WP_UnitTestCase {
 	public function test_hooks_admin_enqueue_scripts() {
 		$this->assertEquals( 10, has_action( 'admin_enqueue_scripts', array( 'c2c_NotificationsForCollapsedAdminMenu', 'add_css' ) ) );
 		$this->assertEquals( 10, has_action( 'admin_enqueue_scripts', array( 'c2c_NotificationsForCollapsedAdminMenu', 'enqueue_js' ) ) );
+	}
+
+	public function test_enqueue_js() {
+		c2c_NotificationsForCollapsedAdminMenu::enqueue_js();
+
+		$this->assertTrue( wp_script_is( 'notifications-for-collapsed-admin-menu' ) );
+	}
+
+	/**
+	 * @dataProvider get_theme_colors
+	 */
+	public function test_add_css( $theme, $color ) {
+		$user_id = $this->create_user( 'author' );
+		update_user_option( $user_id, 'admin_color', $theme );
+
+		$expected = <<<HTML
+		<style type="text/css">
+		.folded #adminmenu li.collapsed-with-pending {
+			background-color:{$color};
+			border-left-color:{$color};
+			border-right-color:{$color};
+		}
+		</style>
+
+HTML;
+
+		c2c_NotificationsForCollapsedAdminMenu::add_css();
+		$this->expectOutputString( $expected );
 	}
 
 }
